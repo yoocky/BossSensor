3cf902d51563f240ef70f56b19ae900458acabbf
zhoumy <zhoumy@xueqiu.com>
2019-09-10 10:52:27 +0800

    add video_capture

---
+++ video_capture.py
@@ -0,0 +1,13 @@
+import cv2 
+
+videoCapture = cv2.VideoCapture()
+videoCapture.open('./data/video/boss.mp4')
+
+fps = videoCapture.get(cv2.CAP_PROP_FPS)
+frames = videoCapture.get(cv2.CAP_PROP_FRAME_COUNT)
+#fps是帧率，意思是每一秒刷新图片的数量，frames是一整段视频中总的图片数量。
+print("fps=",fps,"frames=",frames)
+
+for i in range(int(frames)):
+    ret,frame = videoCapture.read()
+    cv2.imwrite("./data/boss/boss_%d.jpg"%i,frame)
